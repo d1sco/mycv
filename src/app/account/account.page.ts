54170c77172dfdb9e4dd375cd0096d39d0bf4c25
Steve Ford <steveford@mac.mynetworksettings.com>
2025-03-11 15:57:24 -0500

    added firestore

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { UtilsService } from '../utils.service';
 import { Form, FormGroup, FormControl, Validators } from '@angular/forms';
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore"; 
 
 @Component({
   selector: 'app-account',
@@ -12,16 +14,19 @@ import { Form, FormGroup, FormControl, Validators } from '@angular/forms';
 export class AccountPage implements OnInit {
 
   user: any
+  title: string ='Account'
 
   userForm: FormGroup = new FormGroup({
     name: new FormControl(''),
-    email: new FormControl('')
+    phone: new FormControl(''),
   });
 
   constructor(
     private menuController: MenuController,
     private utils: UtilsService
-  ) { }
+  ) { 
+    
+  }
 
   ngOnInit() {
     this.utils.userDetails.subscribe((user) => {
@@ -32,6 +37,25 @@ export class AccountPage implements OnInit {
 
   openMenu(){
     this.menuController.open('main-menu')
+  }
+
+  async onSubmit(){
+    console.log('submit shiet');
+  }
+
+  async addData(){
+    const db = getFirestore();
+    console.log('User form: ', this.userForm.value)
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.log(e)
+    }
   }
 
 }
